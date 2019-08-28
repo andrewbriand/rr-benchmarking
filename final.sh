b9fbb6d653c18062fd259044da582b4eb41badc2
Andrew Briand <atb8888@comcast.net>
2019-08-27 23:31:31 -0700

    Final.sh

---
+++ final.sh
@@ -0,0 +1,6 @@
+/c/Users/atb88/AppData/Local/Programs/Python/Python36/python.exe -u benchmark.py -files egfr_ground_sbml.xml -trials 100 -output_dir egfr-ground-sbml_results/steps-100-start-0-end-50/abriand-laptop -steps 100 -start 0 -end 50
+/c/Users/atb88/AppData/Local/Programs/Python/Python36/python.exe -u benchmark.py -files egfr_ground_sbml.xml -trials 100 -output_dir egfr-ground-sbml_results/steps-1000-start-0-end-50/abriand-laptop -steps 1000 -start 0 -end 50
+/c/Users/atb88/AppData/Local/Programs/Python/Python36/python.exe -u benchmark.py -files egfr_ground_sbml.xml -trials 100 -output_dir egfr-ground-sbml_results/steps-10000-start-0-end-50/abriand-laptop -steps 10000 -start 0 -end 50
+/c/Users/atb88/AppData/Local/Programs/Python/Python36/python.exe -u benchmark.py -files small-test-sbml.xml -trials 100 -output_dir small-test-sbml_results/steps-1000000-start-0-end-50/abriand-laptop -steps 1000000 -start 0 -end 50
+/c/Users/atb88/AppData/Local/Programs/Python/Python36/python.exe -u benchmark.py -files 00645-sbml-13v2.xml -trials 100 -output_dir 00645-sbml-l3v2_results/steps-1000000-start-0-end-50/abriand-laptop -steps 1000000 -start 0 -end 50
+
